484ba3126dc8817849ff79c673a10bee4be41786
TRiley1 <thomas.edward.riley@gmail.com>
2023-09-19 15:04:07 +0100

    feature/ show more button functionality

--- src/components/grumble/GrumbleList.js
+++ src/components/grumble/GrumbleList.js
@@ -1,23 +1,36 @@
 import Grumble from "./Grumble"
 import './GrumbleList.css'
+import React, { useState } from 'react';
+
 
 const GrumbleList = ({grumbles}) => {
 
-    const grumbleNodes = (arr) => {
-        const feed = [];
+    const [noOfMessages, setNoOfMessages] = useState(5);
+
+    const grumbleNodes = (arr, num) => {
+        const grumbles = [];
         for(let grumble of arr){
             const item = <Grumble grumble = {grumble} />
-            feed.unshift(item);
+            grumbles.unshift(item);
         }
+        const feed = grumbles.slice(0,num)
         return feed
+    }
+
+    const handleClick = () => {
+        setNoOfMessages(prev => prev + 5 )
     }
 
 
     return ( 
-        <div className = "grumble-list-container">
-            {grumbleNodes(grumbles)}
-        </div>
-        
+        <>
+            <div className = "grumble-list-container">
+                {grumbleNodes(grumbles, noOfMessages)}
+            </div>
+            <div className="show-more-button">
+                <button onClick={handleClick}>Show More!</button>
+            </div>
+        </>
      );
 }
  
