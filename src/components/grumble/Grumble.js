f6e34d17446003e2c54cf159ce8fa7a71b1ef87e
TRiley1 <thomas.edward.riley@gmail.com>
2023-09-20 15:47:03 +0100

    added frontend dislike connection

--- src/components/grumble/Grumble.js
+++ src/components/grumble/Grumble.js
@@ -1,35 +1,37 @@
-import './Grumble.css'
-
 import React from 'react';
-import './Grumble.css'; // Import the CSS file
+import './Grumble.css';
 
-const Grumble = ({ grumble, authToken, username}) => {
-
-  const renderAgreementMessage = (grumble) => {
-    const likingUsers = grumble.likingUsers;
+const Grumble = ({ grumble, authToken, username }) => {
   
-    if (likingUsers.length === 0) {
+  const renderMessage = (users, messagePrefix, verb) => {
+
+    if (!users || users.length === 0) {
       return null;
-    } else if (likingUsers.length === 1) {
-      return <h3>{`${likingUsers[0].username} likes this`}</h3>;
-    } else if (likingUsers.length === 2) {
-      return (
-        <h3>{`${likingUsers[0].username} & ${likingUsers[1].username} agree`}</h3>
-      );
+    }
+    
+    const firstUser = users[0]?.username;
+
+    if (users.length === 0) {
+      return null;
+    } else if (users.length === 1) {
+      return <h3>{`${firstUser} ${verb} this`}</h3>;
+    } else if (users.length === 2) {
+      const secondUser = users[1]?.username;
+      return <h3>{`${firstUser} & ${secondUser} ${messagePrefix}`}</h3>;
     } else {
-      return <h3>{`${likingUsers[0].username} & ${likingUsers.length - 1} others agree`}</h3>;
+      return <h3>{`${firstUser} & ${users.length - 1} others ${messagePrefix}`}</h3>;
     }
   };
 
-  const handleLikeClick = (grumble, authToken, username ) => {
+  const handleClick = (grumble, authToken, username, likeType) => {
     const body = {
       id: grumble.id,
       username: username,
     };
-  
-    const likeURL = 'http://localhost:8080/grumbles/like';
-  
-    fetch(likeURL, {
+
+    const apiUrl = likeType === 'like' ? 'http://localhost:8080/grumbles/like' : 'http://localhost:8080/grumbles/dislike';
+
+    fetch(apiUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -44,18 +46,12 @@ const Grumble = ({ grumble, authToken, username}) => {
         return response.json();
       })
       .then((data) => {
-        console.log('Liked successfully:', data);
+        console.log(`${likeType.charAt(0).toUpperCase() + likeType.slice(1)}d successfully:`, data);
       })
       .catch((error) => {
-        console.error('Error liking grumble:', error);
+        console.error(`Error ${likeType}ing grumble:`, error);
       });
   };
-
-   const handleDislikeClick = () => {
-
-   }
-  
-  
 
   return (
     <div className="grumble-container">
@@ -64,13 +60,16 @@ const Grumble = ({ grumble, authToken, username}) => {
         <h2>Pending Approval...</h2>
       </div>
       <p className="grumble-text">{grumble.grumble}</p>
-      {renderAgreementMessage(grumble)}
+      {renderMessage(grumble.likingUsers, 'agree', 'likes')}
+      {renderMessage(grumble.disLikingUsers, 'disagree', 'dislikes')}
       <div className="button-container">
-        <button className="like-button" onClick={() => handleLikeClick(grumble, authToken, username)}>Like</button>
-        <button className="dislike-button" onClick={handleDislikeClick}>Dislike</button>
+        <button className="like-button" onClick={() => handleClick(grumble, authToken, username, 'like')}>Like</button>
+        <button className="dislike-button" onClick={() => handleClick(grumble, authToken, username, 'dislike')}>Dislike</button>
       </div>
     </div>
   );
 };
 
 export default Grumble;
+
+
