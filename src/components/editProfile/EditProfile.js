e78f648f3e7e347b957c94c3bb8a02a9e68b67c5
TRiley1 <thomas.edward.riley@gmail.com>
2023-09-25 17:20:11 +0100

    feature/ finished edit profile pic in state

--- src/components/editProfile/EditProfile.js
+++ src/components/editProfile/EditProfile.js
@@ -2,177 +2,172 @@ import React, { useState } from "react";
 import ProfilePic from "../../icons/ProfilePic";
 
 const EditProfile = () => {
-    const options = {
-        "skin colour": {
-          Tanned: "Tanned",
-          Yellow: "Yellow",
-          Pale: "Pale",
-          "Light Brown": "Light Brown",
-          Brown: "Brown",
-          "Dark Brown": "Dark Brown",
-          Black: "Black",
-        },
-        "avatar style" : {"circle": "circle"},
-        "clothe color" : {
-            Black : "Black",
-            Blue01 : "Blue01",
-            Blue02 : "Blue02",
-            Blue03 : "Blue03",
-            Gray01 : "Gray01",
-            Gray02 : "Gray02",
-            Heather : "Heather", 
-            PastelBlue : "PastelBlue",
-            PastelGreen : "PastelGreen", 
-            PastelOrange : "PastelOrange",
-            PastelYellow : "PastelYellow",
-            Pink : "Pink"
-        },
-        "accessories" : {
-            Blank : "Blank",
-            Kurt : "Kurt", 
-            Prescription01 : "Prescription01",
-            Prescription02 : "Prescription02",
-            Round : "Round",
-            Sunglasses : "Sunglasses"
-        },
-        "Clothes" : {
-            BlazerShirt : "BlazerShirt",
-            BlazerSweater : "BlazerSweater",
-            CollarSweater : "CollarSweater",
-            GraphicShirt : "GraphicShirt",
-            Hoodie : "Hoodie",
-            Overall : "Overall",
-            ShirtCrewNeck : "ShirtCrewNeck"
-        },
-        "EyeType" : {
-            Cry : "Cry",
-            EyeRoll : "EyeRoll",
-            Side : "Side",
-            Squint : "Squint",
-            Surprised : "Surprised"
-        },
-        "EyebrowType" : {
-            Angry : "Angry",
-            AngryNatural : "AngryNatural",
-            Default : "Default"
-        },
-        "FacialHair" : {
-            Blank : "Blank",
-            BeardMedium : "BeardMedium",
-            BeardLight : "BeardLight",
-            BeardMajestic : "BeardMajestic",
-            Moustache : "MoustacheFancy"
-        },
-        "FacialHairColour" : {
-            Auburn : "Auburn",
-            Black : "Black",
-            Blonde : "Blonde"
-        },
-        "Hair" : {
-            "Bald" : "NoHair",
-            "Eyepatch" : "Eyepatch",
-            "LongHairBigHair" : "LongHairBigHair",
-            "LongHairBob" : "LongHairBob",
-            "LongHairBun" : "LongHairBun",
-            "LongHairShavedSides" : "LongHairShavedSides",
-            "LongHairMiaWallace" : "LongHairMiaWallace",
-            "ShortHairMullet" : "ShortHairShaggyMullet",
-            "ShortHairCurly" : "ShortHairCurly",
-            "ShortHairShortFlat" : "ShortHairShortFlat",
-            "ShortHairSides" : "ShortHairSides",
-            "ShortHairShortCurly" : "ShortHairShortCurly"
-        },
-        "HairColour" : {
-            "Auburn" : "Auburn",
-            "Black" : "Black",
-            "Blonde" : "Blonde",
-            "Brown" : "Brown"
-        },
-        "MouthType" : {
-            "Disbelief" : "Disbelief",
-            "Grimace" : "Grimace", 
-            "Serious" : "Serious",
-            "Vomit" : "Vomit",
-            "ScreamOpen" : "ScreamOpen"
-        },
-      };
+  const options = {
+    skinColor: {
+      Tanned: "Tanned",
+      Yellow: "Yellow",
+      Pale: "Pale",
+      "Light Brown": "Light Brown",
+      Brown: "Brown",
+      "Dark Brown": "Dark Brown",
+      Black: "Black",
+    },
+    avatarStyle: { Circle: "circle" },
+    clotheColor: {
+      Black: "Black",
+      Blue01: "Blue01",
+      Blue02: "Blue02",
+      Blue03: "Blue03",
+      Gray01: "Gray01",
+      Gray02: "Gray02",
+      Heather: "Heather",
+      PastelBlue: "PastelBlue",
+      PastelGreen: "PastelGreen",
+      PastelOrange: "PastelOrange",
+      PastelYellow: "PastelYellow",
+      Pink: "Pink",
+    },
+    accessories: {
+      Blank: "Blank",
+      Kurt: "Kurt",
+      Prescription01: "Prescription01",
+      Prescription02: "Prescription02",
+      Round: "Round",
+      Sunglasses: "Sunglasses",
+    },
+    clothes: {
+      BlazerShirt: "BlazerShirt",
+      BlazerSweater: "BlazerSweater",
+      CollarSweater: "CollarSweater",
+      GraphicShirt: "GraphicShirt",
+      Hoodie: "Hoodie",
+      Overall: "Overall",
+      ShirtCrewNeck: "ShirtCrewNeck",
+    },
+    eyeType: {
+      Cry: "Cry",
+      EyeRoll: "EyeRoll",
+      Side: "Side",
+      Squint: "Squint",
+      Surprised: "Surprised",
+    },
+    eyebrowType: {
+      Angry: "Angry",
+      AngryNatural: "AngryNatural",
+      Default: "Default",
+    },
+    facialHair: {
+      Blank: "Blank",
+      BeardMedium: "BeardMedium",
+      BeardLight: "BeardLight",
+      BeardMajestic: "BeardMajestic",
+      Moustache: "MoustacheFancy",
+    },
+    facialHairColor: {
+      Auburn: "Auburn",
+      Black: "Black",
+      Blonde: "Blonde",
+    },
+    hair: {
+      Bald: "NoHair",
+      Eyepatch: "Eyepatch",
+      LongHairBigHair: "LongHairBigHair",
+      LongHairBob: "LongHairBob",
+      LongHairBun: "LongHairBun",
+      LongHairShavedSides: "LongHairShavedSides",
+      LongHairMiaWallace: "LongHairMiaWallace",
+      ShortHairMullet: "ShortHairShaggyMullet",
+      ShortHairCurly: "ShortHairCurly",
+      ShortHairShortFlat: "ShortHairShortFlat",
+      ShortHairSides: "ShortHairSides",
+      ShortHairShortCurly: "ShortHairShortCurly",
+    },
+    hairColor: {
+      Auburn: "Auburn",
+      Black: "Black",
+      Blonde: "Blonde",
+      Brown: "Brown",
+    },
+    mouthType: {
+      Disbelief: "Disbelief",
+      Grimace: "Grimace",
+      Serious: "Serious",
+      Vomit: "Vomit",
+      ScreamOpen: "ScreamOpen",
+    },
+  };
 
   const [formData, setFormData] = useState({
-    skinColor: 'Tanned',
-    avatarStyle: 'circle',
-    clotheColor: 'Black',
-    clotheType : 'CollarSweater',
-    accessories: 'Blank',
-    topType : 'ShortHairShortCurly',
-    accessoriesType : 'Kurt',
-    hairColor : "hairColor",
-    facialHairColor : "Black",
-    facialHairType : "MoustacheFancy",
-    eyeType : "EyeType",
+    skinColor: "Tanned",
+    avatarStyle: "Circle",
+    clotheColor: "Black",
+    clothes: "Overall",
+    accessories: "Blank",
+    hair: "ShortHairShortCurly",
+    accessoriesType: "Kurt",
+    hairColor: "Black",
+    facialHairColor: "Black",
+    facialHair: "MoustacheFancy",
+    eyeType: "Default",
     eyebrowType: "Default",
-    mouthType : "Default"
-
-    // Add more attributes and set default values if needed
+    mouthType: "Default",
   });
+  
+  
 
   const handleChange = (e) => {
+    console.log("handleChange called");
     const { name, value } = e.target;
+    console.log(name,value)
     setFormData({ ...formData, [name]: value });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // You can submit the formData to your backend or perform other actions here
     console.log(formData);
+  };
+
+  const renderSelectInput = (label, name, options) => {
+    return (
+      <div key={name}>
+        <label htmlFor={name}>{label}:</label>
+        <select name={name} value={formData[name]} onChange={handleChange}>
+          {Object.keys(options).map((key) => (
+            <option key={key} value={key}>
+              {options[key]}
+            </option>
+          ))}
+        </select>
+      </div>
+    );
   };
 
   return (
     <div>
-        <ProfilePic
-          avatarStyle={formData.avatarStyle}
-          topType={formData.topType}
-          accessoriesType={formData.accessoriesType}
-          hairColor={formData.hairColor}
-          facialHairType={formData.facialHairType}
-          facialHairColor={formData.facialHairColor}
-          clotheType={formData.clotheColor}
-          clotheColor={formData.clotheColor}
-          eyeType={formData.eyeType}
-          eyebrowType={formData.eyebrowType}
-          mouthType={formData.mouthType}
-          skinColor={formData.skinColor}
-        />
+      <ProfilePic
+        avatarStyle={formData.avatarStyle} 
+        topType={formData.hair} 
+        accessoriesType={formData.accessories} 
+        hairColor={formData.hairColor} 
+        facialHairType={formData.facialHair} 
+        facialHairColor={formData.facialHairColor} 
+        clotheType={formData.clothes} 
+        clotheColor={formData.clotheColor} 
+        eyeType={formData.eyeType} 
+        eyebrowType={formData.eyebrowType} 
+        mouthType={formData.mouthType} 
+        skinColor={formData.skinColor} 
+      />
+
       <h2>Edit Profile</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="skinColor">Skin Color:</label>
-          <select name="skinColor" value={formData.skinColor} onChange={handleChange}>
-            {Object.keys(options['skin colour']).map((key) => (
-              <option key={key} value={key}>
-                {options['skin colour'][key]}
-              </option>
-            ))}
-          </select>
-        </div>
-        <div>
-          <label htmlFor="avatarStyle">Avatar Style:</label>
-          <select name="avatarStyle" value={formData.avatarStyle} onChange={handleChange}>
-            <option value="circle">Circle</option>
-          </select>
-        </div>
-        {/* Repeat the above pattern for other attributes */}
-        {/* For example, for clotheColor: */}
-        <div>
-          <label htmlFor="clotheColor">Clothe Color:</label>
-          <select name="clotheColor" value={formData.clotheColor} onChange={handleChange}>
-            {Object.keys(options['clothe color']).map((key) => (
-              <option key={key} value={key}>
-                {options['clothe color'][key]}
-              </option>
-            ))}
-          </select>
-        </div>
-        {/* Add more attribute sections as needed */}
+        {Object.keys(options).map((optionKey) =>
+          renderSelectInput(
+            optionKey,
+            optionKey,
+            options[optionKey]
+          )
+        )}
         <button type="submit">Save</button>
       </form>
     </div>
